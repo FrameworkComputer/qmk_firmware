916e69379dd6b8a744d2197f827c15ae74765f94
Daniel Schaefer <dhs@frame.work>
2022-12-28 04:05:20 +0800

    q1: Enable debug output
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- keyboards/keychron/q1/ansi/keymaps/default/keymap.c
+++ keyboards/keychron/q1/ansi/keymaps/default/keymap.c
@@ -16,6 +16,14 @@
 
 #include QMK_KEYBOARD_H
 
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  debug_keyboard=true;
+  //debug_mouse=true;
+}
+
 enum layers{
     MAC_BASE,
     MAC_FN,
