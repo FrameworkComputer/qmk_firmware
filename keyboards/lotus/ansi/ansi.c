06264abbfa1d0cd5067ba4fa08f2c4a8f95fad62
Daniel Schaefer <dhs@frame.work>
2023-02-08 16:24:48 +0800

    lotus: Move LED to the middle of wide keys
    Currently QMK can only react to one LED per key.
    This looks wrong on wide keys with multiple LEDs.
    
    Only a proble on animations that react to keypresses.
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- keyboards/lotus/ansi/ansi.c
+++ keyboards/lotus/ansi/ansi.c
@@ -295,11 +295,11 @@ const is31_led g_is31_leds[RGB_MATRIX_LED_COUNT] = {
 
 led_config_t g_led_config = { {
   // Key Matrix to LED Index
-  {    31,     72,      5,     84, NO_LED,     29,     33,     77,     75,     80,     78, NO_LED,     88,     79,     52, NO_LED, },
-  {NO_LED, NO_LED, NO_LED,     47,     94,     27,     35,     76,     81,     32, NO_LED, NO_LED,     34,     90,     53,     67, },
+  {    31,     72,      5,     84, NO_LED,     29,     33,     77,     75,     82,     78, NO_LED,     88,     79,     52, NO_LED, },
+  {NO_LED, NO_LED, NO_LED,     47,     96,     27,     35,     76,     81,     30, NO_LED, NO_LED,     34,     90,     53,     67, },
   {NO_LED, NO_LED,     93, NO_LED,      2,     19,     39,     40,     55, NO_LED,     26, NO_LED, NO_LED,     63, NO_LED,     91, },
   {NO_LED,     46,      1, NO_LED,     20,     21,      7,      8,     59, NO_LED,     24,     74, NO_LED, NO_LED, NO_LED,     70, },
-  {NO_LED, NO_LED,     16, NO_LED,     44,     41,      9,     14,     68, NO_LED,     22, NO_LED, NO_LED,     61,     62, NO_LED, },
+  {NO_LED, NO_LED,     16, NO_LED,     38,     41,      9,     14,     68, NO_LED,     22, NO_LED, NO_LED,     61,     62, NO_LED, },
   {NO_LED, NO_LED,     15, NO_LED,     12,     13,     11,     10,     69, NO_LED,     17, NO_LED, NO_LED,     60,     64, NO_LED, },
   {NO_LED, NO_LED, NO_LED, NO_LED,     18,      3,      4,      6,     66, NO_LED,     58,     89, NO_LED,     57,     54, NO_LED, },
   {NO_LED, NO_LED,     36, NO_LED, NO_LED,     25,     43,     49,     51, NO_LED,     50, NO_LED, NO_LED,     48,     37, NO_LED, },
