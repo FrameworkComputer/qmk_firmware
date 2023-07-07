f3ab3b3416f9d338e3b2019906c95096767df2f2
Daniel Schaefer <dhs@frame.work>
2023-07-07 14:00:49 +0800

    fl16: Numpad should have backspace, not delete key
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- keyboards/framework/numpad/keymaps/default/keymap.c
+++ keyboards/framework/numpad/keymaps/default/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * 21 total
      */
     [0] = LAYOUT(
-        KC_ESC,  KC_CALC, KC_EQL,  KC_DEL,
+        KC_ESC,  KC_CALC, KC_EQL,  KC_BSPC,
         KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
         KC_P7,   KC_P8,   KC_P9,
         KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
