9a64719a95687a172961d3ec521566dabbb03df2
Daniel Schaefer <dhs@frame.work>
2023-07-11 02:58:28 +0800

    fl16: Add useful default mapping for gridpad
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- keyboards/framework/gridpad/keymaps/default/keymap.c
+++ keyboards/framework/gridpad/keymaps/default/keymap.c
@@ -82,30 +82,57 @@ led_config_t g_led_config = { {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /*
-     *         ┌────┬────┬────┬────┐
-     *  4 keys │    │    │    │    │
-     *         ├────┼────┼────┼────┤
-     *  4 keys │    │    │    │    │
-     *         ├────┼────┼────┼────┤
-     *  4 keys │    │    │    │    │
-     *         ├────┼────┼────┼────┤
-     *  4 keys │    │    │    │    │
-     *         ├────┼────┼────┼────┤
-     *  4 keys │    │    │    │    │
-     *         ├────┼────┼────┼────┤
-     *  4 keys │    │    │    │    │
-     *         └────┴────┴────┴────┴
+     *         ┌───────┬───────┬───────┬───────┐
+     *  4 keys │ RGB   │RGB Nxt│ RGB + │ Sleep │
+     *         │Toggle │Effect │Brightn│       │
+     *         ├───────┼───────┼───────┼───────┤
+     *  4 keys │Numpad │RGB Prv│ RGB - │       │
+     *         │Layer  │Effect │Brightn│       │
+     *         ├───────┼───────┼───────┼───────┤
+     *  4 keys │ RGB + │ RGB + │ RGB + │  <--  │
+     *         │ Hue   │ Sat   │ Speed │       │
+     *         ├───────┼───────┼───────┼───────┤
+     *  4 keys │ RGB - │ RGB - │ RGB - │ Enter │
+     *         │ Hue   │ Sat   │ Speed │       │
+     *         ├───────┼───────┼───────┼───────┤
+     *  4 keys │       │  Up   │       │       │
+     *         │       │       │       │       │
+     *         ├───────┼───────┼───────┼───────┤
+     *  4 keys │ Left  │ Down  │ Right │       │
+     *         │       │       │       │       │
+     *         └───────┴───────┴───────┴───────┴
      * 24 total
-     *
-     * Default mapping to F keys, because there happen to be just enough to fit.
-     * The user is expected to remap them by recompiling or with VIA.
      */
     [0] = LAYOUT(
-        KC_1,   KC_2,   KC_3,   KC_4,
-        KC_5,   KC_6,   KC_7,   KC_8,
-        KC_9,   KC_0,   KC_A,   KC_B,
-        KC_C,   KC_D,   KC_E,   KC_F,
-        KC_G,   KC_H,   KC_I,   KC_J,
-        KC_K,   KC_L,   KC_M,   KC_N
+        RGB_TOG, RGB_MOD, RGB_VAI, KC_SLEP,
+        TG(1),   RGB_RMOD,RGB_VAD, XXXXXXX,
+        RGB_HUI, RGB_SAI, RGB_SPI, KC_BSPC,
+        RGB_HUD, RGB_SAD, RGB_SPD, KC_ENT,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX
     ),
+     /* Numpad
+     *         ┌────┬────┬────┬────┐
+     *  4 keys │Esc │Calc│ =  │ <- │
+     *         ├────┼────┼────┼────┤
+     *  4 keys │ Num│ /  │ *  │ -  │
+     *         ├────┼────┼────┼────┤
+     *  3 keys │ 7  │ 8  │ 9  │ +  │
+     *         ├────┼────┼────┼────┤
+     *  4 keys │ 4  │ 5  │ 6  │ +  │
+     *         ├────┼────┼────┼────┤
+     *  3 keys │ 1  │ 2  │ 3  │Entr│
+     *         ├────┼────┼────┼────┤
+     *  3 keys │ 0  │ 0  │ .  │Entr│
+     *         └────┼────┴────┴────┴
+     * 21 total
+     */
+    [1] = LAYOUT(
+        KC_ESC,  KC_CALC, KC_EQL,  KC_BSPC,
+        _______, KC_PSLS, KC_PAST, KC_PMNS,
+        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+        KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+        KC_P1,   KC_P2,   KC_P3,   KC_PENT,
+        KC_P0,   KC_P0,   KC_PDOT, KC_PENT
+    )
 };
