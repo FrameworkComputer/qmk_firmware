f2f4de14299e8605b36007dc48c99ee5ebf3fbde
Daniel Schaefer <dhs@frame.work>
2023-07-11 02:02:13 +0800

    fl16: Implement virtual numlock
    Benefits:
    
    - We can put other useful functions on the unused keys
    
    Drawbacks:
    
    - The OS doesn't know when the user is in numlock mode
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- keyboards/framework/numpad/keymaps/default/keymap.c
+++ keyboards/framework/numpad/keymaps/default/keymap.c
@@ -22,10 +22,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [0] = LAYOUT(
         KC_ESC,  KC_CALC, KC_EQL,  KC_BSPC,
-        KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
+        TG(1),   KC_PSLS, KC_PAST, KC_PMNS,
         KC_P7,   KC_P8,   KC_P9,
         KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
         KC_P1,   KC_P2,   KC_P3,
             KC_P0,        KC_PDOT, KC_PENT
     ),
+     /*
+     *         ┌────┬────┬────┬────┐
+     *  4 keys │    │    │    │    │
+     *         ├────┼────┼────┼────┤
+     *  4 keys │    │    │    │    │
+     *         ├────┼────┼────┼────┤
+     *  3 keys │Home│ ↑  │PgUp│    │
+     *         ├────┼────┼────┤    │
+     *  4 keys │ ←  │    │ →  │    │
+     *         ├────┼────┼────┼────┤
+     *  3 keys │End │ ↓  │PdDn│    │
+     *         ├────┴────┼────┤    │
+     *  3 keys │ Insert  │Del │    │
+     *         └─────────┴────┴────┴
+     * 21 total
+     */
+    [1] = LAYOUT(
+        _______, _______, _______, _______,
+        _______, ______,  _______, _______,
+        KC_HOME, KC_UP,   KC_PGUP,
+        KC_LEFT, _______, KC_RGHT, _______,
+        KC_END,  KC_DOWN, KC_PGDN,
+            KC_INS,       KC_DEL,  _______
+
+    )
+
 };
