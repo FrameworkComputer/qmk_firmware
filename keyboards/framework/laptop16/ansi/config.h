39ffa2458f3f1199d901c725d82d8729250b11fb
Daniel Schaefer <dhs@frame.work>
2023-07-27 13:09:24 +0800

    fl16: Port ansi keyboard to IS31FL3741A controller
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- keyboards/framework/laptop16/ansi/config.h
+++ keyboards/framework/laptop16/ansi/config.h
@@ -4,13 +4,11 @@
 #define MATRIX_COLS 16
 #define MATRIX_ROWS 8
 
-#define DRIVER_COUNT 2
-#define DRIVER_ADDR_1 0b0100000
-#define DRIVER_ADDR_2 0b0100011
-#define DRIVER_1_LED_TOTAL 48
-#define DRIVER_2_LED_TOTAL 49
-#define RGB_MATRIX_LED_COUNT (DRIVER_1_LED_TOTAL + DRIVER_2_LED_TOTAL)
-#define ISSI_CONFIGURATION 0x21 // Enable only the first 9 SW and disable software shutdown
+#define DRIVER_COUNT 1
+#define DRIVER_ADDR_1 0b0110000
+#define RGB_MATRIX_LED_COUNT 97
+ // Disable software shutdown (SW1-SW9 enabled)
+#define ISSI_CONFIGURATION 0x01
 
 // Limit current to ensure max current draw is just about 500mA
 // when white att 100% brightness
