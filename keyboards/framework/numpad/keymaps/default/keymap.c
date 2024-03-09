83c211b667ed50d47d7c57832c0544ff0d840b41
Carl Kittelberger <icedream@icedream.pw>
2024-03-09 22:10:38 +0100

    fl16: Use the correct key code for numpad equal.
    Fixes FrameworkComputer/qmk_firmware#30.

--- keyboards/framework/numpad/keymaps/default/keymap.c
+++ keyboards/framework/numpad/keymaps/default/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * 21 total
      */
     [_NUMLOCK] = LAYOUT(
-        KC_ESC,  KC_CALC, KC_EQL,  KC_BSPC,
+        KC_ESC,  KC_CALC, KC_PEQL, KC_BSPC,
         KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
         KC_P7,   KC_P8,   KC_P9,
         KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
